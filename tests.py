f2c6336337cce74c7c1108fb0b6f8ba6778bef14
jaymody <jaykmody@gmail.com>
2022-09-25 22:52:30 +0200

    Use global var seed in tests.py.

--- tests.py
+++ tests.py
@@ -10,6 +10,8 @@ from model import (
     transformer_predict_fn,
 )
 
+_SEED = 123
+
 
 class BlockJaxKeyReuse:
     """Context manager that throws an error if a key is reused by a jax.random function.
@@ -77,20 +79,20 @@ class BlockJaxKeyReuse:
 def test_BlockJaxKeyReuse():
     with pytest.raises(AssertionError):
         with BlockJaxKeyReuse():
-            key = jax.random.PRNGKey(123)
+            key = jax.random.PRNGKey(_SEED)
             a = jax.random.normal(key, (2, 5))
             b = jax.random.normal(key, (2, 5))
 
     with pytest.raises(AssertionError):
         with BlockJaxKeyReuse():
-            key = jax.random.PRNGKey(123)
+            key = jax.random.PRNGKey(_SEED)
             key, subkey = jax.random.normal(key, (2, 5))
-            key = jax.random.PRNGKey(123)
+            key = jax.random.PRNGKey(_SEED)
             key, subkey = jax.random.normal(key, (2, 5))
 
     with pytest.raises(AssertionError):
         with BlockJaxKeyReuse():
-            key = jax.random.PRNGKey(123)
+            key = jax.random.PRNGKey(_SEED)
             key, a_subkey, b_subkey = jax.random.split(key, 3)
             a = jax.random.normal(key, (2, 5))
             b = jax.random.normal(a_subkey, (2, 5))
@@ -99,27 +101,27 @@ def test_BlockJaxKeyReuse():
 
     # the following tests should not throw any errors
     with BlockJaxKeyReuse():
-        key = jax.random.PRNGKey(123)
+        key = jax.random.PRNGKey(_SEED)
         key, a_subkey, b_subkey = jax.random.split(key, 3)
         a = jax.random.normal(a_subkey, (2, 5))
         b = jax.random.normal(b_subkey, (2, 5))
 
     with BlockJaxKeyReuse():
-        key = jax.random.PRNGKey(123)
+        key = jax.random.PRNGKey(_SEED)
         key, subkey = jax.random.split(key)
         a_subkey, b_subkey = jax.random.split(subkey)
         a = jax.random.normal(a_subkey, (2, 5))
         b = jax.random.normal(b_subkey, (2, 5))
 
     with BlockJaxKeyReuse():
-        key = jax.random.PRNGKey(123)
+        key = jax.random.PRNGKey(_SEED)
         key, a_subkey, b_subkey = jax.random.split(key, 3)
         a = jax.random.normal(key, (2, 5))
         b = jax.random.normal(a_subkey, (2, 5))
         c = jax.random.normal(b_subkey, (2, 5))
 
     with BlockJaxKeyReuse():
-        key = jax.random.PRNGKey(123)
+        key = jax.random.PRNGKey(_SEED)
         key, subkey = jax.random.split(key)
         key, subkey = jax.random.split(key)
 
@@ -128,7 +130,7 @@ def test_initialization_does_not_reuse_keys():
     """Tests that we didn't reuse any keys during initialization."""
     with BlockJaxKeyReuse():
         initialize_transformer_params(
-            seed=123,
+            seed=_SEED,
             src_vocab_size=200,
             trg_vocab_size=300,
             d_model=512,
@@ -141,7 +143,7 @@ def test_initialization_does_not_reuse_keys():
 
 def test_forward_fn_and_predict_fn():
     params = initialize_transformer_params(
-        seed=123,
+        seed=_SEED,
         src_vocab_size=200,
         trg_vocab_size=300,
         d_model=512,

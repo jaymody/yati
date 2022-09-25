7c764fa4263cb2ac90cefcb3b5cb845f0ad54bb0
jaymody <jaykmody@gmail.com>
2022-09-25 18:43:14 +0200

    Fix tests.

--- tests.py
+++ tests.py
@@ -38,29 +38,45 @@ def test_initialization_is_random():
         b = jnp.asarray(jnp.unique(a, return_counts=True)).T
         return b[b[:, 1].argsort()][::-1]
 
-    _seed = 111
+    _seed = 127
     weights = initialize_transformer_params(
-        seed=127,
-        src_vocab_size=10,
-        trg_vocab_size=20,
+        seed=_seed,
+        src_vocab_size=5,
+        trg_vocab_size=10,
         d_model=8,
         d_ff=16,
-        h=2,
-        n_enc_layers=3,
-        n_dec_layers=3,
+        h=4,
+        n_enc_layers=2,
+        n_dec_layers=2,
     )
     weights_flattened = jnp.array(flatten_and_concat(weights))
     num_params = weights_flattened.shape[0]
     print(f"num params: {num_params}\n")
     print(value_counts(weights_flattened)[:20])
+    value_counts_dict = {float(k): int(v) for k, v in value_counts(weights_flattened)}
 
+    # remove 0.0 and 1.0 since those are repeated from bias + layer norm initializations
+    del value_counts_dict[0.0]
+    del value_counts_dict[1.0]
+
+    # see assert message below
+    assert all(v == 1 for v in value_counts_dict.values()), (
+        "Some weights are repeated, this may not mean there is a bug. This check is "
+        "dependent on the initialization code. If a change to the code was made, this "
+        "test can break even if the initialization code doens't reuse keys. Try "
+        "manually running this test with a different seed to make sure the same "
+        "frequencies don't show up."
+    )
+
+    # for comparison, generate a list of random numbers with the same number of
+    # parameters and see if it has repeated values
     key = jax.random.PRNGKey(_seed)
     test = jax.random.normal(key, (num_params,))
     print()
     print(value_counts(test)[:20])
 
 
-def test_initialization_forward_fn_and_predict_fn():
+def test_forward_fn_and_predict_fn():
     params = initialize_transformer_params(
         seed=123,
         src_vocab_size=200,
